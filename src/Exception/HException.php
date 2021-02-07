44289778aa0ec184d4fb313ad8ae7c278856d189
dimon dominator <vilya04@gmail.com>
2021-02-08 01:00:59 +0200

    init

---
+++ src/Exception/HException.php
@@ -0,0 +1,10 @@
+<?php
+
+
+class HException extends Exception
+{
+    public function __construct(string $message)
+    {
+        parent::__construct($message);
+    }
+}
--- src/HDirectory.php
+++ src/HDirectory.php
@@ -1,9 +1,11 @@
 <?php
 
-//namespace
-
 class HDirectory
 {
+    public const DS = '/';
+
+    public const ABSENT_DIR_ERR = '%s -- dir is absent.';
+
     private string $path;
 
     public function __construct(string $path)
@@ -14,5 +16,44 @@ class HDirectory
     public function getPath(): string
     {
         return $this->path;
+    }
+
+    public function getFilesList(): array
+    {
+        $this->checkDirExists();
+
+        $files = [];
+        foreach (scandir($this->path) as $contentElement) {
+            if ($contentElement !== '.' && $contentElement !== '..') {
+                if (is_file($this->path . self::DS . $contentElement)) {
+                    $files[] = $contentElement;
+                }
+            }
+        }
+
+        return $files;
+    }
+
+    public function getDirsList(): array
+    {
+        $this->checkDirExists();
+
+        $dirs = [];
+        foreach (scandir($this->path) as $contentElement) {
+            if ($contentElement != '.' && $contentElement != '..') {
+                if (is_dir($this->path . self::DS . $contentElement)) {
+                    $dirs[] = $contentElement;
+                }
+            }
+        }
+
+        return $dirs;
+    }
+
+    private function checkDirExists(): void
+    {
+        if (!is_dir($this->path)) {
+            throw new HException(sprintf(self::ABSENT_DIR_ERR, $this->path));
+        }
     }
 }
