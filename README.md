ab347f2670816636a0ad608f7f92f4e3586e503d
dimon dominator <vilya04@gmail.com>
2021-02-08 00:30:01 +0200

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+PHP micro-functional helper.
---
+++ src/HDirectory.php
@@ -0,0 +1,18 @@
+<?php
+
+//namespace
+
+class HDirectory
+{
+    private string $path;
+
+    public function __construct(string $path)
+    {
+        $this->path = $path;
+    }
+
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+}
