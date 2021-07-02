4b21c22d57bdba182d7c04add386122f67b9ff06
dimon dominator <vilya04@gmail.com>
2021-07-03 00:33:13 +0300

    added: arrayDiffAssocRecursive

--- function/array.php
+++ function/array.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Put every array element into an array (format of PHPUnit data provider).
+ * Put every array element into an array (format of PHPUnit Data Provider).
  * If element is array, handle its name to the Test as well.
  *
  * @param array $array
@@ -21,4 +21,34 @@ function wrap(array $array): array
     }
 
     return $wrapped;
+}
+
+/**
+ * @param array $arrayA
+ * @param array $arrayB
+ * @return array
+ *
+ * @tested 1.4.3
+ */
+function arrayDiffAssocRecursive(array $arrayA, array $arrayB): array
+{
+    ksort($arrayA);
+    ksort($arrayB);
+
+    $diff = [];
+    foreach ($arrayA as $key => $value) {
+        if (is_array($value)) {
+            if (!isset($arrayB[$key]) || !is_array($arrayB[$key])) {
+                $diff[$key] = $value;
+            } else {
+                $newDiff = arrayDiffAssocRecursive($value, $arrayB[$key]);
+                if (!empty($newDiff))
+                    $diff[$key] = $newDiff;
+            }
+        } else if (!array_key_exists($key, $arrayB) || $arrayB[$key] !== $value) {
+            $diff[$key] = $value;
+        }
+    }
+
+    return $diff;
 }
